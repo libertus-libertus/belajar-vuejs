41c3842f6dc1ee30269b869d824f1dfe1114db12
libertus-libertus <libert.jobs@gmail.com>
2024-11-16 12:31:39 +0700

    Instalasi framework vuejs dan tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
